9a67e979d05fd6caf379a0a59b186f0c6b1aa554
Sara Thompson <s34thomp@gmail.com>
2025-05-03 13:18:07 -0400

    Added pagination

--- src/components/ItemIndexLayout/ItemIndexLayout.jsx
+++ src/components/ItemIndexLayout/ItemIndexLayout.jsx
@@ -1,10 +1,21 @@
 import './ItemIndexLayout.css'
-
-import { Link } from 'react-router-dom';
-
 import { AllData } from '../../data/AllData';
 
+import React, { useState, useMemo } from 'react';
+import { Link } from 'react-router-dom';
+
+const ITEMS_PER_PAGE = 12;
+
 function ItemIndexLayout() {
+    const entries = useMemo(() => Object.entries(AllData), []);     // Creates an array consisting of [ {<name>, <value>}, ...]
+    const pageCount = Math.ceil(entries.length / ITEMS_PER_PAGE);   // Calculates total number of pages required
+
+    const [currentPage, setCurrentPage] = useState(1);
+
+    const start = (currentPage - 1) * ITEMS_PER_PAGE;
+    const currentEntries = useMemo(() => {                          // Creates a list of entries for the current page selected
+        return entries.slice(start, start + ITEMS_PER_PAGE);
+      }, [entries, currentPage]);
 
     return (
         <div className="content-parent">
@@ -14,15 +25,28 @@ function ItemIndexLayout() {
             <div className='item-index'>
                 <div className="divider-down"></div>
                 <div className="item-list">
-                    {Object.entries(AllData).map( ([slug, item]) => (
+                    {/* {Object.entries(AllData).map( ([slug, item]) => (
                         <Link to={`/items/${slug}`} className='item-link' key={slug}>
                             <img className="link-image" src={item.iconURL} alt="shield" />
                             <span className="link-label">{item.name}</span>
+                        </Link>
+                    ))} */}
+                    {currentEntries.map(([slug, item]) => (
+                        <Link to={`/items/${slug}`} className="item-link" key={slug}>
+                        <img className="link-image" src={item.iconURL} alt={item.name} />
+                        <span className="link-label">{item.name}</span>
                         </Link>
                     ))}
                 </div>
                 <div className="divider-up"></div>
             </div>
+
+            {/* Pagination goes here. */}
+            <div className="pagination">
+                <button className='paginationButton' onClick={() => setCurrentPage(prev => prev === 1 ? prev : prev - 1)}>&lt; Back</button>
+                <button className='paginationButton' onClick={() => setCurrentPage(prev => prev === pageCount ? prev : prev + 1)}>Next &gt;</button>
+            </div>
+            <p className="pagination">{start+1} - {start+12 < entries.length ? start+12 : entries.length} of {entries.length} results</p>
         </div>
     );
 }
